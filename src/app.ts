cb804cb15f30e350fd13e1ec6ed6ce16ec15e7d2
unknown <mukosiy909@gmail.com>
2026-01-21 15:41:22 +0200

    refactor: configure routing and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import { animeRouter } from './modules/anime/anime.routes';
+import { reviewsRouter } from './modules/reviews/reviews.routes';
+
+import { listRouter } from './modules/list/list.routes';
+import { errorHandler } from './common/errors/errorHandler';
+import { notFound } from './common/errors/notFound';
 
 const app = express();
-
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'OK',
-        message: 'Animelist API is running'
-    });
+    res.status(200).json({ status: 'OK', message: 'Animelist API is running' });
 });
+
+// Anime
+app.use('/anime', animeRouter);
+
+// Reviews
+app.use('/anime/:id/reviews', reviewsRouter);
+// List
+app.use('/me/list', listRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
